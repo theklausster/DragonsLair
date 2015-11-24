85fae9f8324084a6968edb5fbfd57cf0322b0583
Jakobsen <theklausster@gmail.com>
2015-11-24 12:56:31 +0100

    test

--- DragonLairBackend/BackendTest/TestEntitity/TestGroup.cs
+++ DragonLairBackend/BackendTest/TestEntitity/TestGroup.cs
@@ -11,17 +11,61 @@ namespace BackendTest.TestEntitity
     [TestFixture]
     class TestGroup
     {
+        int id = 1;
+        string name = "Grp 1";
+        Group group;
+        [SetUp]
+        public void SetUp()
+        {
+            group = new Group() { Id = id, Name = name };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            group = null;
+        }
+
 
 
         [Test]
         public void Test_if_attributes_can_be_set()
-        {
-            int id = 1;
-            string name = "Grp 1";
-            Group group = new Group() { Id = id, Name = name };
+        {  
+            //Attributes are in "SetUp" 
             Assert.AreEqual(group.Id, id);
             Assert.AreEqual(group.Name, name);
-            
+        }
+
+        [Test]
+        public void Test_if_group_is_in_a_turney()
+        {
+            int id = 1;
+            string name = "Tournament 1";
+            DateTime startDate = DateTime.Today;
+            group.Tournament = new Tournament() { Id = id, Name = name, StartDate = startDate };
+            Assert.NotNull(group.Tournament);
+            Assert.AreEqual(group.Tournament.Id, id);
+        }
+
+        [Test]
+        public void Test_if_three_teams_can_be_added_to_one_group()
+        {
+            int id = 1;
+            string name = "Team One";
+            int win = 1;
+            int loss = 2;
+            int draw = 3;
+            Team team = new Team() { Id = id, Name = name, Draw = draw, Loss = loss, Win = win };
+            Team team2 = new Team() { Id = id, Name = name, Draw = draw, Loss = loss, Win = win };
+            Team team3 = new Team() { Id = id, Name = name, Draw = draw, Loss = loss, Win = win };
+            group.Teams = new List<Team> { team, team2, team3 };
+            Assert.NotNull(group.Teams);
+            Assert.Greater(group.Teams.Count, 0);
+            Assert.AreEqual(team, group.Teams[0]);
+            Assert.AreEqual(team2, group.Teams[1]);
+            Assert.AreEqual(team3, group.Teams[2]);
+
+
         }
     }
 }
--- DragonLairBackend/BackendTest/TestEntitity/TestTournament.cs
+++ DragonLairBackend/BackendTest/TestEntitity/TestTournament.cs
@@ -11,18 +11,68 @@ namespace BackendTest.TestEntitity
     [TestFixture]
     class TestTournament
     {
+        int id = 1;
+        string name = "Tournament 1";
+        DateTime startDate = DateTime.Today;
+        Tournament tournament;
+
+        [SetUp]
+        public void SetUp()
+        {
+            tournament = new Tournament() { Id = id, Name = name, StartDate = startDate };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            tournament = null;
+        }
+
         [Test]
         public void Test_if_attributes_can_be_set()
         {
-            int id = 1;
-            string name = "Tournament 1";
-            DateTime startDate = DateTime.Today;
-
-
-            Tournament tournament = new Tournament() { Id = id, Name = name, StartDate = startDate };
+            // Attributes is set in "SetUp"
             Assert.AreEqual(tournament.Id, id);
             Assert.AreEqual(tournament.Name, name);
             Assert.AreEqual(tournament.StartDate, startDate);
+        }
+
+
+        [Test]
+        public void Test_if_tournament_can_have_two_groups()
+        {
+            int id = 1;
+            string name = "Grp 1";
+            Group group = new Group() { Id = id, Name = name };
+            Group group2 = new Group() { Id = id, Name = name };
+            tournament.Groups = new List<Group>() { group, group2 };
+            Assert.NotNull(tournament.Groups);
+            Assert.Greater(tournament.Groups.Count, 0);
+            Assert.AreEqual(tournament.Groups[0], group);
+            Assert.AreEqual(tournament.Groups[1], group2);
+
+        }
+
+        [Test]
+        public void Test_if_tournament_has_a_game()
+        {
+            int id = 1;
+            string name = "Game 1";
+            Game game = new Game() { Id = id, Name = name };
+            tournament.Game = game;
+            Assert.NotNull(tournament.Game);
+            Assert.AreEqual(tournament.Game.Id, id);
+        }
+
+        [Test]
+        public void Test_if_tournament_has_a_tournamentType()
+        {
+            int id = 1;
+            string type = "War Hammer";
+            TournamentType tournementType = new TournamentType() { Id = id, Type = type };
+            tournament.TournamentType = tournementType;
+            Assert.NotNull(tournament.TournamentType);
+            Assert.AreEqual(tournementType.Type, type);
         }
     }
 }
