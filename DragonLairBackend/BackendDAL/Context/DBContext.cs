5972ddeb2465bd825c4dbd74cf4918a42d9bff73
Suresokken <jigzie@gmail.com>
2015-11-25 08:17:59 +0100

    small

---
+++ DragonLairBackend/BackendDAL/Context/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using BackendDAL.Initializer;
+using Entities;
+
+namespace BackendDAL.Context
+{
+    public class DBContext : DbContext
+    {
+
+        public DBContext() : base("DragonLair")
+        {
+            Database.SetInitializer(new DragonLairInitializer());
+            
+        }
+        public DbSet<Player> Players { get; set; }
+    }
+}
