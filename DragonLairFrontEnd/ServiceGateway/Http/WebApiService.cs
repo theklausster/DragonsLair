6bc3330e726b94eec33a06f4756c4d440d472f83
Suresokken <jigzie@gmail.com>
2015-11-27 08:39:01 +0100

    spring cleaning

--- DragonLairFrontEnd/ServiceGateway/Http/WebApiService.cs
+++ DragonLairFrontEnd/ServiceGateway/Http/WebApiService.cs
@@ -15,26 +15,5 @@ namespace ServiceGateway.Http
             }
             throw new ApiException(response.StatusCode, json);
         }
-
-        //    using (var client = new BearerHttpClient())
-            //    {
-            //        if (!bearerToken.IsNullOrWhiteSpace())
-            //        {
-            //            //Add the authorization header
-            //            client.DefaultRequestHeaders.Authorization =
-            //                AuthenticationHeaderValue.Parse("Bearer " + bearerToken);
-            //        }
-
-            //        var ApiException = await client.GetAsync(BuildActionUri(action));
-
-            //        string json = await ApiException.Content.ReadAsStringAsync();
-            //        if (ApiException.IsSuccessStatusCode)
-            //        {
-            //            return JsonConvert.DeserializeObject<T>(json);
-            //        }
-
-            //        throw new ApiException(ApiException.StatusCode, json);
-            //    }
-
-        }
+    }
 }
