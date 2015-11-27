c05b737a9f3a606dc87f675da25d223a3f3bf044
Jakobsen <theklausster@gmail.com>
2015-11-27 08:44:09 +0100

    webdetails

--- DragonLairBackend/BackendDAL/Repositories/GameRepository.cs
+++ DragonLairBackend/BackendDAL/Repositories/GameRepository.cs
@@ -9,11 +9,46 @@ using BackendDAL.Context;
 namespace BackendDAL.Repositories
 {
 
-    public class GameRepository : AbstractRepository<Game>
+    public class GameRepository : IRepository<Game>
     {
-        public override bool Update(Game entity)
+        public Game Create(Game entity)
         {
-                using (var context = new DragonLairContext())
+            using (var context = new DragonLairContext())
+            {
+                context.Games.Add(entity);
+                context.SaveChanges();
+            }
+            return entity;
+        }
+
+        public void Delete(int id)
+        {
+            using (var context = new DragonLairContext())
+            {
+                context.Games.Remove(context.Games.Find(id));
+                context.SaveChanges();
+            }
+        }
+
+        public Game Read(int id)
+        {
+            using (var context = new DragonLairContext())
+            {
+                return context.Games.Find(id);
+            }
+        }
+
+        public IEnumerable<Game> ReadAll()
+        {
+            using (var context = new DragonLairContext())
+            {
+                return context.Games.ToList();
+            }
+        }
+
+        public bool Update(Game entity)
+        {
+            using (var context = new DragonLairContext())
             {
                 Game game = context.Games.Find(entity.Id);
                 if ((game == null)) return false;
@@ -24,56 +59,6 @@ namespace BackendDAL.Repositories
                 return true;
             }
         }
-
-
-        //public Game Create(Game entity)
-        //{
-        //    using (var context = new DragonLairContext())
-        //    {
-        //        context.Games.Add(entity);
-        //        context.SaveChanges();
-        //    }
-        //    return entity;
-        //}
-
-        //public void Delete(int id)
-        //{
-        //    using (var context = new DragonLairContext())
-        //    {
-        //        context.Games.Remove(context.Games.Find(id));
-        //        context.SaveChanges();
-        //    }
-        //}
-
-        //public Game Read(int id)
-        //{
-        //    using (var context = new DragonLairContext())
-        //    {
-        //        return context.Games.Find(id);
-        //    }
-        //}
-
-        //public IEnumerable<Game> ReadAll()
-        //{
-        //    using (var context = new DragonLairContext())
-        //    {
-        //        return context.Games.ToList();
-        //    }
-        //}
-
-        //    public bool Update(Game entity)
-        //    {
-        //        using (var context = new DragonLairContext())
-        //        {
-        //            Game game = context.Games.Find(entity.Id);
-        //            if ((game == null)) return false;
-        //            game.Name = entity.Name;
-        //            game.Genre = entity.Genre;
-        //            game.Tournaments = entity.Tournaments;
-        //            context.SaveChanges();
-        //            return true;
-        //        }
-        //    }
-        //}
     }
+}
 }
