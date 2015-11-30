d4b7676ea1a6b19bf2a18862d90c33d6db8edafe
Glenn Hansen <Borxigar@hotmail.com>
2015-11-30 12:17:38 +0100

    GroupDtoExtended

--- DragonLairBackend/BackendTest/TestDTOConverter/TestDTOGroupConverter.cs
+++ DragonLairBackend/BackendTest/TestDTOConverter/TestDTOGroupConverter.cs
@@ -16,8 +16,14 @@ namespace BackendTest.TestDTOConverter
         [Test]
         public void Test_if_Group_with_team_and_tournaments_can_be_converted_to_DTO()
         {
-            Team Team1 = new Team() { Id = 1, Name = "Awesome"};
-            Team Team2 = new Team() { Id = 1, Name = "More Awesome" };
+            Player player = new Player() {Id = 1, Name = "Søren"};
+            Player player2 = new Player() { Id = 2, Name = "Lars" };
+            Player player3 = new Player() { Id = 3, Name = "Peter" };
+            Player player4 = new Player() { Id = 4, Name = "Poul" };
+            List<Player> PlayersForTeam1 = new List<Player>() {player, player2};
+            List<Player> PlayersForTeam2 = new List<Player>() { player3, player4 };
+            Team Team1 = new Team() { Id = 1, Name = "Awesome", Players = PlayersForTeam1};
+            Team Team2 = new Team() { Id = 1, Name = "More Awesome", Players = PlayersForTeam2 };
             Tournament tournament1 = new Tournament() { Id = 1, Name = "Warhammer" };
             List<Team> teams = new List<Team> { Team1, Team2 };
             Group group = new Group() { Id = 1, Name = "A", Teams = teams, Tournament = tournament1 };
