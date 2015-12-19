e78e982bad5625caaa58f8ee7392ef1c7468d082
Suresokken <jigzie@gmail.com>
2015-12-19 21:01:03 +0100

    fixingTest

--- DragonLairFrontEnd/FrontEndIntegrationTest/IntegrationsTest/TestTournamentViewModel.cs
+++ DragonLairFrontEnd/FrontEndIntegrationTest/IntegrationsTest/TestTournamentViewModel.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace TestTournamentLogic
+namespace FrontEndIntegrationTest.IntegrationsTest
 {
     [TestFixture]
     class TestTournamentViewModel
